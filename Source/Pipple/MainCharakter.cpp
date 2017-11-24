e321ad9e9c2bc8bd2b1d316d53cb147b731447bd
Markus Mutas <markusmutas@googlemail.com>
2017-11-24 14:57:09 +0100

    deleted wrong code

--- Source/Pipple/MainCharakter.cpp
+++ Source/Pipple/MainCharakter.cpp
@@ -54,36 +54,3 @@ void AMainCharakter::StopJump() {
 	bPressedJump = false;
 }
 
-//Initializing the USpringArmComponent attribute
-SpringArm = PCIP.CreateDefaultSubobject<USpringArmComponent>(this, TEXT("CameraBoom"));
-
-//Adding the springArm to the Character's RootComponent (the collision capsule)
-SpringArm->AttachTo(RootComponent);
-
-//bAbsoluteRotation allows us to define if this support will rotate with the player or will stay fixed.
-//In our case we don't want it to rotate with the character.
-SpringArm->bAbsoluteRotation = true;
-
-//The distance between the arm and its target. This value defines the distance between the character and the camera.
-//Try out different values to see the outcome.
-SpringArm->TargetArmLength = 500.f;
-
-//Socket Offset.
-//Socket is an anchor point to other components.
-//For instance, in the character case we can define the socket in the character's hand
-//this way we can add another component(for example a gun).
-//But in our case we will add a camera to our SpringArm
-SpringArm->SocketOffset = FVector(0.f, 0.f, 75.f);
-
-//The relative rotation of the arm regard to its parent.
-//We want the camera rotated 180 degrees in the Y axis in order to be situated parallel to the character, at the same level.
-//This way we achieve the classic side-scroller camera's style.
-SpringArm->RelativeRotation = FRotator(0.f, 180.f, 0.f);
-
-// Creating the UCameraComponent instance.
-SideViewCamera = PCIP.CreateDefaultSubobject<UCameraComponent>(this, TEXT("SideViewCamera"));
-
-//The AttachTo method allow us to add an object to another in a given socket. It receives two parameters,
-//the first one, the object where we will be anchored (the springArm) and the second the socket's name where we will be anchored.
-//USpringArmComponent's SocketName returns the name of the components socket.
-SideViewCamera->AttachTo(SpringArm, USpringArmComponent::SocketName);
