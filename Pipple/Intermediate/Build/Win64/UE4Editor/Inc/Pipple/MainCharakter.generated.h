15db21eaaf0ad3b4fcbda87a1f48657d13d4d4b4
Markus Mutas <markusmutas@googlemail.com>
2017-11-24 01:37:09 +0100

    Added custom MainCharacter class & settings

---
+++ Pipple/Intermediate/Build/Win64/UE4Editor/Inc/Pipple/MainCharakter.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PIPPLE_MainCharakter_generated_h
+#error "MainCharakter.generated.h already included, missing '#pragma once' in MainCharakter.h"
+#endif
+#define PIPPLE_MainCharakter_generated_h
+
+#define Pipple_Source_Pipple_MainCharakter_h_12_RPC_WRAPPERS
+#define Pipple_Source_Pipple_MainCharakter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define Pipple_Source_Pipple_MainCharakter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMainCharakter(); \
+	friend PIPPLE_API class UClass* Z_Construct_UClass_AMainCharakter(); \
+public: \
+	DECLARE_CLASS(AMainCharakter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/Pipple"), NO_API) \
+	DECLARE_SERIALIZER(AMainCharakter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Pipple_Source_Pipple_MainCharakter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMainCharakter(); \
+	friend PIPPLE_API class UClass* Z_Construct_UClass_AMainCharakter(); \
+public: \
+	DECLARE_CLASS(AMainCharakter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/Pipple"), NO_API) \
+	DECLARE_SERIALIZER(AMainCharakter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Pipple_Source_Pipple_MainCharakter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMainCharakter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMainCharakter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMainCharakter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMainCharakter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMainCharakter(AMainCharakter&&); \
+	NO_API AMainCharakter(const AMainCharakter&); \
+public:
+
+
+#define Pipple_Source_Pipple_MainCharakter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMainCharakter(AMainCharakter&&); \
+	NO_API AMainCharakter(const AMainCharakter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMainCharakter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMainCharakter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMainCharakter)
+
+
+#define Pipple_Source_Pipple_MainCharakter_h_12_PRIVATE_PROPERTY_OFFSET
+#define Pipple_Source_Pipple_MainCharakter_h_9_PROLOG
+#define Pipple_Source_Pipple_MainCharakter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Pipple_Source_Pipple_MainCharakter_h_12_PRIVATE_PROPERTY_OFFSET \
+	Pipple_Source_Pipple_MainCharakter_h_12_RPC_WRAPPERS \
+	Pipple_Source_Pipple_MainCharakter_h_12_INCLASS \
+	Pipple_Source_Pipple_MainCharakter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Pipple_Source_Pipple_MainCharakter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Pipple_Source_Pipple_MainCharakter_h_12_PRIVATE_PROPERTY_OFFSET \
+	Pipple_Source_Pipple_MainCharakter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	Pipple_Source_Pipple_MainCharakter_h_12_INCLASS_NO_PURE_DECLS \
+	Pipple_Source_Pipple_MainCharakter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Pipple_Source_Pipple_MainCharakter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Pipple/Intermediate/Build/Win64/UE4Editor/Inc/Pipple/Pipple.init.gen.cpp
+++ Pipple/Intermediate/Build/Win64/UE4Editor/Inc/Pipple/Pipple.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodePipple_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/Pipple",
 				PKG_CompiledIn | 0x00000000,
-				0x08FCD6C1,
-				0x89E86FBA,
+				0x2DAB38BC,
+				0xA18E03C8,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
